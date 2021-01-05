504650e975f924cfc08efa1cab19ca5fd4eac27d
Niro-dev <nirb1992@gmail.com>
2021-01-05 11:26:34 +0200

    Add project files.

---
+++ CinemaProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CinemaProject.Startup))]
+namespace CinemaProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
